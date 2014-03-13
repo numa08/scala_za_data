67a858316c4a4a429ad4bcc26ac45563ffa44166
numa08 <n511287@gmail.com>
2014-03-13 23:37:59 +0900

    サンプル画像を削除

--- 8th/js/custom.js
+++ 8th/js/custom.js
@@ -1,1 +1,33 @@
-/* custom behavior */
+/* custom behavior *//* custom behavior */
+(function  () {
+  var contents = document.getElementsByClassName('content');
+	var link = function  () {
+			var a = document.createElement('a');
+			a.href = "https://twitter.com/numa08";
+			a.innerText = "@numa08";
+			var div = document.createElement('div');
+			div.style.cssText = "float:left";
+			div.appendChild(a);
+			return div;
+		};
+
+	var hashtag = function  () {
+			var div = document.createElement('div');
+			div.innerText = "#新宿Scala座";
+			return div;
+		};
+ 
+ 
+	var footer = function(){
+			var content = document.createElement('div');
+			content.style.cssText = "text-align:right";
+			content.appendChild(link());
+			content.appendChild(hashtag());
+			return content;
+		};
+ 
+	for (var i = contents.length - 1; i >= 0; i--) {
+		console.log(i);
+		contents[i].appendChild(footer());
+	};
+})();
