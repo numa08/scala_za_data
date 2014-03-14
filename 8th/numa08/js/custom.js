0729e16de8c5d6b079af3e4bdc33743cc7cde5c4
numa08 <n511287@gmail.com>
2014-03-15 01:38:02 +0900

    フォントやレイアウトの調整

--- 8th/numa08/js/custom.js
+++ 8th/numa08/js/custom.js
@@ -1,33 +1,63 @@
 /* custom behavior *//* custom behavior */
 (function  () {
-  var contents = document.getElementsByClassName('content');
-	var link = function  () {
-			var a = document.createElement('a');
-			a.href = "https://twitter.com/numa08";
-			a.innerText = "@numa08";
-			var div = document.createElement('div');
-			div.style.cssText = "float:left";
-			div.appendChild(a);
-			return div;
-		};
-
-	var hashtag = function  () {
-			var div = document.createElement('div');
-			div.innerText = "#新宿Scala座";
-			return div;
-		};
- 
- 
-	var footer = function(){
-			var content = document.createElement('div');
-			content.style.cssText = "text-align:right";
-			content.appendChild(link());
-			content.appendChild(hashtag());
-			return content;
-		};
- 
-	for (var i = contents.length - 1; i >= 0; i--) {
-		console.log(i);
-		contents[i].appendChild(footer());
-	};
+	window.addEventListener("load", function() {
+		var script = document.getElementsByTagName('head')[0].getElementsByTagName('script')[0];
+		script.setAttribute("src", "https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js");
+	}, false);		
 })();
+
+$(function(){
+	//Add Footer
+	$("#slides").prepend(function  () {
+		var link = $("<div>", {
+			"class" : "left",
+		}).append($("<a>", {
+			target  : "_blank",
+			href 	: "https://twitter.com/numa08",
+			text    : "@numa08"
+		}));
+
+		var hashtag = $("<ul>")
+		.append($("<li>",{
+			"class" : "hashtag"
+		})
+		.append($("<img>", {
+			"src"	: "https://g.twimg.com/Twitter_logo_blue.png",
+			"class" : "logo"
+		}))
+		.append($("<a>", {
+			text : "#新宿Scala座",
+			target : "_blank",
+			href : "https://twitter.com/intent/tweet?text=Scala%20on%20Android&hashtags=新宿Scala座&url=" + encodeURIComponent(window.location.origin  +window.location.pathname)
+		})));
+
+		var footer = $("<div>", {
+			"class" : "footer",
+		}).append(link)
+		.append(hashtag);
+
+		return footer;
+	}());
+
+	//Add Header
+	$("#slides").prepend(function  () {
+		var slideTitle = $("<div>", {
+			"class" : "left"
+		}).append($("<a>", {
+			"class" : "topbar_link",
+			text : "Scala on Android",
+			href : "http://scala.numa08.net"
+		}));
+		var eventTitle = $("<a>", {
+			"class" : "topbar_link right",
+			text : "新宿Scala座 '14年２月号",
+			href : "http://scala.numa08.net"
+		});
+		var topbar = $("<div>", {
+			"class" : "topbar"
+		}).append(slideTitle)
+		.append(eventTitle);
+
+		return topbar;
+	}());
+});
